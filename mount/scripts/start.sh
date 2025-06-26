bc8d4569b7f24b8fd1b52a7ca731f3e12114b504
Sajidur Rahman <sajidur.inbox@gmail.com>
2025-06-27 02:06:05 +0600

    added: alembic with user and role table

--- mount/scripts/start.sh
+++ mount/scripts/start.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+# Print a message indicating that the migration is starting
+echo "Running Alembic migrations..."
+
+# Run Alembic migrations to upgrade the database schema to the latest revision
+alembic upgrade head
+
+# Check if the migration was successful
+if [ $? -ne 0 ]; then
+  echo "Alembic migrations failed."
+  # exit 1
+fi
+
 # Print a message indicating that the FastAPI server is starting
 echo "Starting FastAPI server..."
 
