8cd565fcf96ce648da577a313bb0b8549b9614bc
Sajidur Rahman <sajidur.inbox@gmail.com>
2025-06-27 00:52:51 +0600

    added: initial router

--- mount/app/main.py
+++ mount/app/main.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI
 
+from app.api.v1 import router as v1_router
+
 app = FastAPI()
 
-@app.get("/")
-def read_root():
-    return {"message": "Hello from FastAPI + PostgreSQL!"}
+@app.get("/", tags=["Root"])
+def docs():
+    return {"message": "Root endpoint for the API. Use /docs for documentation."}
+
+app.include_router(v1_router, prefix='/api/v1')
